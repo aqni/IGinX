a4d6fe27758b652c8e4d248fbbcb0b42347537f1
An Qi <aqnin@outlook.com>
2024-07-02 10:58:05 +0800

    style: add license headers

--- test/src/test/resources/pySessionIT/tests.py
+++ test/src/test/resources/pySessionIT/tests.py
@@ -1,3 +1,21 @@
+#
+# IGinX - the polystore system with high performance
+# Copyright (C) Tsinghua University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 # 无法单独执行，用来测试PySessionIT
 import sys, traceback
 sys.path.append('../session_py/')  # 将上一级目录添加到Python模块搜索路径中
