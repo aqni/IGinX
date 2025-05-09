9feaa6aec498f1c5c59a056a39f3daeac732cdb3
An Qi <aqnin@outlook.com>
2025-05-09 19:58:38 +0800

    feat(mongo): set default value of schema.sample.size to 1

--- dataSource/mongodb/src/main/java/cn/edu/tsinghua/iginx/mongodb/MongoDBStorage.java
+++ dataSource/mongodb/src/main/java/cn/edu/tsinghua/iginx/mongodb/MongoDBStorage.java
@@ -82,7 +82,7 @@ public class MongoDBStorage implements IStorage {
   public static final String CONNECTION_STRING = "uri";
   public static final String SCHEMA_SAMPLE_SIZE = "schema.sample.size";
   public static final String QUERY_SAMPLE_SIZE = "dummy.sample.size";
-  public static final String SCHEMA_SAMPLE_SIZE_DEFAULT = "1000";
+  public static final String SCHEMA_SAMPLE_SIZE_DEFAULT = "1";
   public static final String QUERY_SAMPLE_SIZE_DEFAULT = "0";
   public static final String BOUNDARY_LEVEL = "boundary.level";
   public static final String BOUNDARY_LEVEL_DEFAULT = "0";
