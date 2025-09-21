e00c8a5a832337ee40ab4773513e4e2bef0e5dae
An Qi <aqnin@outlook.com>
2025-09-21 16:03:48 +0800

    feat(filesystem): add new structure named `FileLsm` to prepare for extending file format of storage engine

---
+++ dataSource/filesystem/src/main/java/cn/edu/tsinghua/iginx/filesystem/struct/lsm/util/arrow/ArrowFieldTypes.java
@@ -0,0 +1,49 @@
+/*
+ * IGinX - the polystore system with high performance
+ * Copyright (C) Tsinghua University
+ * TSIGinX@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package cn.edu.tsinghua.iginx.filesystem.struct.lsm.util.arrow;
+
+import cn.edu.tsinghua.iginx.thrift.DataType;
+import org.apache.arrow.util.Preconditions;
+import org.apache.arrow.vector.types.pojo.DictionaryEncoding;
+import org.apache.arrow.vector.types.pojo.FieldType;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class ArrowFieldTypes {
+
+  public static FieldType of(
+      boolean nullable,
+      DataType dataType,
+      @Nullable DictionaryEncoding dictionary,
+      @Nullable Map<String, String> metadata) {
+    Preconditions.checkNotNull(dataType, "dataType");
+
+    return new FieldType(nullable, ArrowTypes.of(dataType), dictionary, metadata);
+  }
+
+  public static FieldType nonnull(DataType dataType, @Nullable Map<String, String> metadata) {
+    return of(false, dataType, null, null);
+  }
+
+  public static FieldType nonnull(DataType dataType) {
+    return nonnull(dataType, null);
+  }
+}
