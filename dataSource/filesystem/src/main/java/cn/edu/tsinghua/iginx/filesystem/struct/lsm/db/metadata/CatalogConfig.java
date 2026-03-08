d85dff1bcca58072db4a7f7bb09ee30939063770
An Qi <aqnin@outlook.com>
2026-03-08 19:24:49 +0800

    feat: update schemaIndexType to use PREFIX_TAG_TREE

--- dataSource/filesystem/src/main/java/cn/edu/tsinghua/iginx/filesystem/struct/lsm/db/metadata/CatalogConfig.java
+++ dataSource/filesystem/src/main/java/cn/edu/tsinghua/iginx/filesystem/struct/lsm/db/metadata/CatalogConfig.java
@@ -33,7 +33,7 @@ import lombok.experimental.FieldNameConstants;
 @FieldNameConstants
 public class CatalogConfig extends AbstractConfig {
 
-  @Optional FieldIndexType schemaIndexType = FieldIndexType.FLAT;
+  @Optional FieldIndexType schemaIndexType = FieldIndexType.PREFIX_TAG_TREE;
 
   @Override
   public List<ValidationProblem> validate() {
